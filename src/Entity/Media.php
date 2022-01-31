38abedebc8212b0a2b3252c6cb30ebde34203cef
Corenthin FLAMAND <corenthin.flamand@gmail.com>
2022-01-31 15:22:47 +0100

    changing

---
+++ src/Entity/Media.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\Table;
+
+#[Table(name: 'corenthin_projet6_media')]
+#[Entity()]
+
+class Media
+{
+    #[Id]
+    #[Column(type: 'integer')]
+    #[GeneratedValue(strategy: 'AUTO')]
+    private int $id;
+
+    #[Column(type: 'string')]
+    private string $type;
+
+    #[Column(type: 'string')]
+    private string $path;
+
+    #[Column]
+}
