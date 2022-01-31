40c75588c9806cf0b2ac1f3b074768627f1c0c28
Corenthin FLAMAND <corenthin.flamand@gmail.com>
2022-01-31 14:57:08 +0100

    done

---
+++ src/Entity/Category.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\Table;
+
+#[Table(name: 'corenthin_projet6_category')]
+#[Entity()]
+
+class Category
+{
+    #[Id]
+    #[Column(type: 'integer')]
+    #[GeneratedValue(strategy: 'AUTO')]
+    private int $id;
+
+    #[Column(type: 'string')]
+    private string $name;
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
