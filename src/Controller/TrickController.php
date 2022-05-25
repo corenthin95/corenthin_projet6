4229bf9b6bf843041c79ac2cea7ed6aa441219ff
Corenthin FLAMAND <corenthin.flamand@gmail.com>
2022-05-25 13:39:23 +0200

    done

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -31,6 +31,7 @@ class TrickController extends AbstractController
     {
         $trick = $trickRepository->findOneBy(['slug' => $slug]);
         $comment = $commentRepository->findCommentByTrick($trick->getId(), 1);
+        $totalComments = $trick->getComment()->count();
 
         $form = $this->formFactory->create(CommentType::class)
                                   ->handleRequest($request);
@@ -49,6 +50,7 @@ class TrickController extends AbstractController
         return $this->render('tricks/trick.html.twig', [
             'trick' => $trick,
             'comment' => $comment,
+            'totalComments' => $totalComments,
             'form' => $form->createView()
         ]);
     }
